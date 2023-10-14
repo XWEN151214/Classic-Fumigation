067b3d7a2d942b9dc94aba25e69338edb12cd502
unknown <muhammadali30804@gmail.com>
2023-10-14 19:51:58 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,21 +27,24 @@ const dropDown = () => {
     );
 }
 
-const Services = [{link: "bed-bug-treatment", title: "Bed-Bug Treatment"},
-    { link: "carpet-cleaning-service", title: "Carpet Cleaning" },
-    { link: "coakroahes-control-service", title: "Coackroaches Control Service" },
-    { link: "disinfection-services", title: "Disinfection Services" },
-    { link: "fumigation-services", title: "Fumigation Services" },
-    { link: "mosquito-spray-service", title: "Mosquito Spray Service" },
-    { link: "pest-control", title: "Pest Control" },
-    { link: "rodent-control", title: "Rodent Control" },
-    { link: "sofa-cleaning-services", title: "Sofa Cleaning Services" },
-    { link: "termite-control", title: "Termite Control" }]
+const Services = [{ id: 1, link: "bed-bug-treatment", title: "Bed-Bug Treatment" },
+{ id: 2, link: "carpet-cleaning-services", title: "Carpet Cleaning Service" },
+{ id: 3, link: "coakroahes-control-services", title: "Coackroaches Control Service" },
+{ id: 4, link: "disinfection-services", title: "Disinfection Services" },
+{ id: 5, link: "fumigation-services", title: "Fumigation Services" },
+{ id: 6, link: "mosquito-spray-service", title: "Mosquito Spray Service" },
+{ id: 7, link: "pest-control", title: "Pest Control" },
+{ id: 8, link: "rodent-control", title: "Rodent Control" },
+{ id: 9, link: "sofa-cleaning-services", title: "Sofa Cleaning Services" },
+{ id: 10, link: "termite-control", title: "Termite Control" }]
 
 const Navbar = () => {
     const [isDropped, setIsDropped] = useState(false);
     const [navOpen, setNavOpen] = useState(false);
 
+    const toggleDropdown = () => {
+        setIsDropped(!isDropped);
+    };
     return (
         <nav className="navbar p-0 m-0">
             <div className="nav-info container-fluid d-flex align-items-center justify-content-center py-4">
@@ -70,21 +73,36 @@ const Navbar = () => {
                     <li className="mx-3">
                         <Link to="/about">About Us</Link>
                     </li>
-                    <li className="mx-3" onClick={() => showDropDown(isDropped, setIsDropped)}>
+                    {/* <li className="mx-3" onClick={() => showDropDown(isDropped, setIsDropped)}>
                         <a className="dropdown-toggle" href="#">
                             Services
                             <span className="caret"></span>
                         </a>
                         {
                             isDropped ? <div className="dropmenu">
-                                {Services.map((service)=> {
-                                    <li className="py-1 px-3">
+                                {Services.map((service) => {
+                                    <li className="py-1 px-3" key={service.id}>
                                         <Link to={`/services/${service.link}`}>{service.title}</Link>
                                     </li>
                                 })}
                             </div>
                                 : <></>
                         }
+                    </li> */}
+                    <li className="mx-3" onClick={toggleDropdown}>
+                        <a className="dropdown-toggle" href="#">
+                            Services
+                            <span className="caret"></span>
+                        </a>
+                        {isDropped && (
+                            <ul className="dropmenu">
+                                {Services.map((service) => (
+                                    <li className="py-1 px-3" key={service.id}>
+                                        <Link to={`/services/${service.link}`}>{service.title}</Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
                     </li>
                     <li className="mx-3">
                         <Link to="/clients">Clients</Link>
@@ -107,25 +125,36 @@ const Navbar = () => {
                             <li className="mx-3 mt-3">
                                 <Link to="/about">About Us</Link>
                             </li>
-                            <li className="mx-3 mt-3" onClick={() => showDropDown(isDropped, setIsDropped)}>
+                            {/* <li className="mx-3 mt-3" onClick={() => showDropDown(isDropped, setIsDropped)}>
                                 <a className="dropdown-toggle" href="#">
                                     Services
                                     <span className="caret"></span>
                                 </a>
                                 {
-                                    isDropped ? <div className="mobile-dropmenu py-2">
-                                        <li className="py-1">
-                                            <Link to="/services/bed-bug-treatment">Bed Bug Treatment</Link>
-                                        </li>
-                                        <li className="py-1">
-                                            <a href="#">Page 1-2</a>
-                                        </li>
-                                        <li className="py-1">
-                                            <a href="#">Page 1-3</a>
-                                        </li>
+                                    isDropped ? <div className="dropmenu">
+                                        {Services.map((service) => {
+                                            <li className="py-1 px-3" key={service.id}>
+                                                <Link to={`/services/${service.link}`}>{service.title}</Link>
+                                            </li>
+                                        })}
                                     </div>
                                         : <></>
                                 }
+                            </li> */}
+                            <li className="mx-3" onClick={toggleDropdown}>
+                                <a className="dropdown-toggle" href="#">
+                                    Services
+                                    <span className="caret"></span>
+                                </a>
+                                {isDropped && (
+                                    <ul className="dropmenu">
+                                        {Services.map((service) => (
+                                            <li className="py-1 px-3" key={service.id}>
+                                                <Link to={`/services/${service.link}`}>{service.title}</Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                )}
                             </li>
                             <li className="mx-3 mt-3">
                                 <Link to="/clients">Clients</Link>
